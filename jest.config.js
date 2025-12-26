5595a93bde060e94d9838dc80ccebbec7b99ba1a
vfcp-dev <victorcastrop@correo.unicordoba.edu.co>
2025-12-25 19:56:34 -0500

    feat: remover Karma completamente e instalar Jest limpio
    - Removidos karma.conf.js y src/test.ts
    - Desinstaladas dependencias de Karma
    - Instalado jest-preset-angular@14
    - Configuración limpia de Jest
    - Tests detectados, requieren ajustes finales

--- jest.config.js
+++ jest.config.js
@@ -1,36 +1,33 @@
-// Jest configuration without preset
 module.exports = {
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
     testEnvironment: 'jsdom',
-    roots: ['<rootDir>/src'],
-    testMatch: ['**/*.spec.ts'],
-    moduleFileExtensions: ['ts', 'js', 'html'],
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+            stringifyContentPathRegex: '\\.(html|svg)$',
+        },
+    },
+    coverageDirectory: 'coverage',
+    transform: {
+        '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    },
+    transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+    snapshotSerializers: [
+        'jest-preset-angular/build/serializers/no-ng-attributes',
+        'jest-preset-angular/build/serializers/ng-snapshot',
+        'jest-preset-angular/build/serializers/html-comment',
+    ],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/www/'],
+    moduleNameMapper: {
+        '^@app/(.*)$': '<rootDir>/src/app/$1',
+        '^@environments/(.*)$': '<rootDir>/src/environments/$1',
+    },
     collectCoverageFrom: [
         'src/**/*.ts',
         '!src/**/*.spec.ts',
         '!src/main.ts',
         '!src/polyfills.ts',
-        '!src/test.ts',
-        '!src/environments/**'
+        '!src/environments/**',
     ],
-    coverageDirectory: 'coverage',
-    transform: {
-        '^.+\\.ts$': ['ts-jest', {
-            tsconfig: '<rootDir>/tsconfig.spec.json',
-            stringifyContentPathRegex: '\\.html$',
-            isolatedModules: true
-        }]
-    },
-    moduleNameMapper: {
-        '^@app/(.*)$': '<rootDir>/src/app/$1',
-        '^@environments/(.*)$': '<rootDir>/src/environments/$1'
-    },
-    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    testPathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-        '<rootDir>/dist/',
-        '<rootDir>/www/'
-    ],
-    transformIgnorePatterns: [
-        'node_modules/(?!(@angular|@ionic|@ngrx|rxjs)/)'
-    ]
 };
