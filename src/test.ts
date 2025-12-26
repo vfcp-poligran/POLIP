eecf40b330c2bbed86bcd8f2581d66d6a2bfbd78
vfcp-dev <victorcastrop@correo.unicordoba.edu.co>
2025-12-25 19:19:30 -0500

    docs: plan Fase 2 - Funcionalidades Core

--- src/test.ts
+++ src/test.ts
@@ -12,3 +12,8 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
+
+// Then we find all the tests.
+const context = (require as any).context('./', true, /\.spec\.ts$/);
+// And load the modules.
+context.keys().forEach(context);
