0691fcbd544d7c534e07470b94717e9d8a615921
vfcp-dev <victorcastrop@correo.unicordoba.edu.co>
2025-12-25 20:10:40 -0500

    fix: Remove redundant `setupZoneTestEnv` call and add Jest setup clarification comments.

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,3 +1,4 @@
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
-
-setupZoneTestEnv();
+// Setup file for Jest
+// NOTE: Do NOT import 'jest-preset-angular/setup-jest' here
+// @angular-builders/jest already imports it automatically
+// Importing it again causes "Cannot set base providers" error
